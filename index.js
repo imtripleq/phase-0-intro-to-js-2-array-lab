60edb2e5185a38d6441d667febc3333533b60f55
imtripleq <flamework89@hotmail.com>
2021-10-27 12:16:29 +1100

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+const destructivelyAppendCat = () => {
+  cats.push("Ralph");
+};
+
+const destructivelyPrependCat = () => {
+  cats.unshift("Bob");
+};
+
+const destructivelyRemoveLastCat = () => {
+  cats.pop();
+};
+
+const destructivelyRemoveFirstCat = () => {
+  cats.shift();
+};
+
+const appendCat = (n) => {
+  const y = cats.slice();
+  y.push(n);
+  return y;
+};
+
+const prependCat = (n) => {
+  const y = cats.slice();
+  y.unshift(n);
+  return y;
+};
+
+const removeLastCat = () => {
+  const y = cats.slice(0, 2);
+  return y;
+};
+
+const removeFirstCat = () => {
+  const y = cats.slice(1);
+  return y;
+};
